e1298b7b26e9f553edf853d258e59033606da295
Maurice Zandberg <maurice.zandberg@twict.nl>
2022-07-28 14:25:32 +0200

    Setup Springboot project.

---
+++ Back-end/sqooshj/src/main/java/SKBD/sqooshj/SqooshjApplication.java
@@ -0,0 +1,13 @@
+package SKBD.sqooshj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqooshjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqooshjApplication.class, args);
+	}
+
+}
---
+++ Back-end/sqooshj/src/test/java/SKBD/sqooshj/SqooshjApplicationTests.java
@@ -0,0 +1,13 @@
+package SKBD.sqooshj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqooshjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
